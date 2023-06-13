6368f8c3dc31ef98495b16c957b879ecddafde06
aelligp <93140290+aelligp@users.noreply.github.com>
2023-06-13 15:18:44 +0000

    Deploying to SS23-output from @ UniMainzGeo/JuliaIntro@68faf9f7a2656d9101ea05ad05ece69ce773fd30 🚀

---
+++ src/sidebar data.jl
@@ -0,0 +1,9 @@
+Dict(
+    "main" => [
+        "Welcome" => collections["welcome"].pages,
+        # "Module 1: GeophysicalModelGenerator.jl" => collections["module1"].pages,
+        # "Module 2: Social Science & Data Science" => collections["module2"].pages,
+        # "Module 3: Climate Science" => collections["module3"].pages,
+
+    ],
+)
